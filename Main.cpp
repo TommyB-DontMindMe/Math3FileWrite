5dd328f4b55d8188e4bcd5c795e8a82f0a02df49
TommyB-DontMindMe <142879081+TommyB-DontMindMe@users.noreply.github.com>
2025-01-30 13:38:51 +0100

    Implemented Task 2

--- Main.cpp
+++ Main.cpp
@@ -36,6 +36,8 @@ float SolvePolynom(const vector<float> Coefficients, float Indeterminate)
 
 int main()
 {
+	int Assignment = 0;
+
 	int PolynomialDegree = 0;
 	vector<float> PolynomCoefficients;
 
@@ -47,48 +49,77 @@ int main()
 
 	string Input;
 
-	cout << "Please select polynomial degree:" << endl;
-	while (PolynomialDegree == 0)
+	std::cout << "Please select task:" << endl;
+	while (Assignment == 0)
 	{
 		cin >> Input;
 
 		try
 		{
-			PolynomialDegree = stoi(Input);
+			Assignment = stoi(Input);
 		}
 		catch (const std::exception&)
 		{
-			cout << "Unable to parse, please try again." << endl;
+			std::cout << "Unable to parse, please try again." << endl;
 		}
-		if (PolynomialDegree <= 0)
+		if (Assignment >= 3)
 		{
-			cout << "I do not know how to handle a polynom with " << PolynomialDegree << " terms" << endl;
-			cout << "Please try again." << endl;
+			std::cout << "There is currently no task 3 or above." << endl;
+			std::cout << "Please try again" << endl;
+			Assignment = 0;
 		}
 	}
-
-	for (size_t i = 0; i <= PolynomialDegree; i++)
+	switch (Assignment)
 	{
-		cout << "Please specify " << (i < 1 ? "constant" : Ordinal(i) + " coefficient") << endl;
-
-		bool ValidCoefficient = false;
-		while (!ValidCoefficient)
+	case 1:
+		std::cout << "Please select polynomial degree:" << endl;
+		while (PolynomialDegree == 0)
 		{
 			cin >> Input;
+
 			try
 			{
-				PolynomCoefficients.push_back(stof(Input));
+				PolynomialDegree = stoi(Input);
 			}
 			catch (const std::exception&)
 			{
-				cout << "Unable to parse, please try again." << endl;
+				std::cout << "Unable to parse, please try again." << endl;
 			}
-			ValidCoefficient = true;
+			if (PolynomialDegree <= 0)
+			{
+				std::cout << "I do not know how to handle a polynom with " << PolynomialDegree << " terms" << endl;
+				std::cout << "Please try again." << endl;
+			}
 		}
+
+		for (size_t i = 0; i <= PolynomialDegree; i++)
+		{
+			std::cout << "Please specify " << (i < 1 ? "constant" : Ordinal(i) + " coefficient") << endl;
+
+			bool ValidCoefficient = false;
+			while (!ValidCoefficient)
+			{
+				cin >> Input;
+				try
+				{
+					PolynomCoefficients.push_back(stof(Input));
+				}
+				catch (const std::exception&)
+				{
+					std::cout << "Unable to parse, please try again." << endl;
+				}
+				ValidCoefficient = true;
+			}
+		}
+		break;
+
+	default:
+		break;
 	}
+	
 
 	// Determine start and end values
-	cout << "Specify starting value" << endl;
+	std::cout << "Specify starting value" << endl;
 	bool ValidInput = false;
 	while (!ValidInput)
 	{
@@ -99,11 +130,11 @@ int main()
 		}
 		catch (const std::exception&)
 		{
-			cout << "Unable to parse, please try again." << endl;
+			std::cout << "Unable to parse, please try again." << endl;
 		}
 		ValidInput = true;
 	}
-	cout << "Now, specify end value" << endl;
+	std::cout << "Now, specify end value" << endl;
 	ValidInput = false;
 	while (!ValidInput)
 	{
@@ -114,13 +145,13 @@ int main()
 		}
 		catch (const std::exception&)
 		{
-			cout << "Unable to parse, please try again." << endl;
+			std::cout << "Unable to parse, please try again." << endl;
 		}
 		ValidInput = true;
 	}
 
 	// Determine intervals
-	cout << "Decide amount of intervalls" << endl;
+	std::cout << "Decide amount of intervalls" << endl;
 	ValidInput = false;
 	while (!ValidInput)
 	{
@@ -131,7 +162,7 @@ int main()
 		}
 		catch (const std::exception&)
 		{
-			cout << "Unable to parse, please try again." << endl;
+			std::cout << "Unable to parse, please try again." << endl;
 		}
 		ValidInput = true;
 	}
@@ -141,39 +172,78 @@ int main()
 	MyFile.open("Test.txt", ios::out);
 	if (MyFile.is_open())
 	{
-		MyFile << Input << endl;
+		MyFile << Resolution * 2 << endl;
 
 		float CurrentValue = FunctionStart;
-		float Result = SolvePolynom(PolynomCoefficients, CurrentValue);
-		for (size_t i = 1; i <= Resolution; i++)
+
+		switch (Assignment)
 		{
-			Vertex StartVertex;
-			StartVertex.x = CurrentValue;
-			StartVertex.y = Result;
-
-			CurrentValue = FunctionStart + i * Interval;
-			float NewResult = SolvePolynom(PolynomCoefficients, CurrentValue);
-			Vertex EndVertex;
-			EndVertex.x = CurrentValue;
-			EndVertex.y = NewResult;
-
-			if (NewResult < Result)
+		case 1:
+		{
+			float Result = SolvePolynom(PolynomCoefficients, CurrentValue);
+			for (size_t i = 1; i <= Resolution; i++)
 			{
-				StartVertex.r = 1;
-				EndVertex.r = 1;
+				Vertex StartVertex;
+				StartVertex.x = CurrentValue;
+				StartVertex.y = Result;
+
+				CurrentValue = FunctionStart + i * Interval;
+				float NewResult = SolvePolynom(PolynomCoefficients, CurrentValue);
+				Vertex EndVertex;
+				EndVertex.x = CurrentValue;
+				EndVertex.y = NewResult;
+
+				if (NewResult < Result)
+				{
+					StartVertex.r = 1;
+					EndVertex.r = 1;
+				}
+				else
+				{
+					StartVertex.g = 1;
+					EndVertex.g = 1;
+				}
+
+
+				MyFile << StartVertex << endl;
+				MyFile << EndVertex << endl;
+
+				Result = NewResult;
 			}
-			else
-			{
-				StartVertex.g = 1;
-				EndVertex.g = 1;
-			}
-
-
-			MyFile << StartVertex << endl;
-			MyFile << EndVertex << endl;
-
-			Result = NewResult;
 		}
+			break;
+		case 2:
+		{
+			Vertex VertexA;
+
+			VertexA.x = cos(CurrentValue);
+			VertexA.y = sin(CurrentValue);
+			VertexA.z = CurrentValue;
+
+			VertexA.r = 1.0f;
+			VertexA.b = 1.0f;
+
+			for (size_t i = 1; i <= Resolution; i++)
+			{
+				Vertex VertexB;
+
+				CurrentValue += Interval;
+
+				VertexB.x = cos(CurrentValue);
+				VertexB.y = sin(CurrentValue);
+				VertexB.z = CurrentValue;
+
+				MyFile << VertexA << endl;
+				MyFile << VertexB << endl;
+
+				VertexA = VertexB;
+			}
+		}
+			break;
+		default:
+			break;
+		}
+		
 		MyFile.close();
 	}
 	// if (N < N-1) { Color = (1, 0, 0) }
